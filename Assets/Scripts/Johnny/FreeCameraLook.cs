2ed7811fee8ea47be9c9d9ac8611bd9b6413a168
Johnny <shenjiaming2003@gmail.com>
2025-04-02 19:25:21 -0400

    Update FreeCameraLook.cs

--- Assets/Scripts/Johnny/FreeCameraLook.cs
+++ Assets/Scripts/Johnny/FreeCameraLook.cs
@@ -134,7 +134,7 @@ public class FreeCameraLook : Pivot {
 	private void UpdateTarget() 
 	{
 		Vector3 centerScreen = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f);
-		Ray forwardRay = cam.ScreenPointToRay(centerScreen);
+		Ray forwardRay = cam.GetComponent<Camera>().ScreenPointToRay(centerScreen);
 		// Ray forwardRay = cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 		Debug.DrawRay(forwardRay.origin, forwardRay.direction * 1000f, Color.red, 0.1f);
 
